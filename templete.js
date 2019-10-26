074e09e2b762be93812d2a1125e6bee8a45facb9
pww21 <56424299+pww21@users.noreply.github.com>
2019-10-26 00:26:15 -0400

    Add files via upload

---
+++ templete.js
@@ -0,0 +1,5 @@
+function choose(){
+var choicespot=document.getElementsByClassName("menueContents");
+var choices=["About Me","Prodjects","Contact Me"];
+choicespot[0].innerHTML="<div class='choosing'><a href='aboutMe.html'>"+choices[0]+"</a></div><div class='choosing'><a href='projects.html'>"+choices[1]+"</a></div><div class='choosing'><a href='#'>"+choices[2]+"</a></div>";
+}
