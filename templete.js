7f549e045933870c167fa1cad9014f5fd638dd51
pww21 <56424299+pww21@users.noreply.github.com>
2019-10-26 01:11:43 -0400

    Update templete.js

--- templete.js
+++ templete.js
@@ -2,5 +2,5 @@ function choose(){
 var choicespot=document.getElementsByClassName("menueContents");
 var choices=["About Me","Projects"];
 choicespot[0].innerHTML="<div class='choosing'><a href='index.html'>"+choices[0]+"</a></div>
-  <div class='choosing'><a href='projects.html'>"+choices[1]+"</a></div>";
+  <div class='choosing'><a href='projects.html'>"+choices[1]"</a></div>";
 }
